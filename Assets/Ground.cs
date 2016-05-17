d0a8d50bb03c526306a42c3b3b021bf221ec7049
Ben Scheirman <ben@scheirman.com>
2016-05-17 10:13:34 -0500

    Ground, grass, base

---
+++ Assets/Ground.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class Ground : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		// transform.renderer.material.mainTextureScale = new Vector2(XScale , YScale 
+
+		var meshRenderer = GetComponent<MeshRenderer>();
+
+		meshRenderer.sharedMaterial.mainTextureScale = new Vector2 (20, 20);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
