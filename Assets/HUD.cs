820015fed87521dfb2dffce363825bce28f81bb4
Jonathan Birkholz <rookieone@gmail.com>
2016-05-24 14:44:56 -0500

    used layouts to show multiple selections and buttons for hud

--- Assets/HUD.cs
+++ Assets/HUD.cs
@@ -6,17 +6,18 @@ using System.Collections.Generic;
 public class HUD : MonoBehaviour {
 
 	public Component minimap;
-	public Component selectionPanel;
+	public RectTransform selectionPanel;
 	public RectTransform actionsPanel;
 
-	public Text selectionNameText;
-
 	public GameObject actionButtonPrefab;
+	public GameObject selectedWobPrefab;
 
 	WorldObject[] selection;
 	List<GameObject> actionButtons;
+	List<GameObject> selectedWobObjects;
 
 	void Start () {
+		selectedWobObjects = new List<GameObject> ();
 		ClearSelection ();
 	}
 
@@ -28,36 +29,44 @@ public class HUD : MonoBehaviour {
 		selection = wobs;
 
 		if (wobs.Length == 0) {
-			selectionNameText.text = string.Empty;
+//			selectionNameText.text = string.Empty;
 		} else if (wobs.Length == 1) {
 			ConfigureHUDForWorldObject (wobs [0]);
-
+			AddSelectedWob (wobs [0]);
 		} else {
-			selectionNameText.text = "Multiple Selection";
 			// if all units are the same type, we can show the single selection UI
 		}
 	}
 
+	void AddSelectedWob(WorldObject wob) {
+		var selectedWobObj = (GameObject)Instantiate (selectedWobPrefab);
+		var script = selectedWobObj.GetComponent<SelectedWobScript> ();
+		script.SetWorldObject (wob);
+
+		selectedWobObj.transform.SetParent (selectionPanel, false);
+		selectedWobObj.transform.localScale = new Vector3 (1, 1, 1);
+		selectedWobObjects.Add (selectedWobObj);
+	}
+
 	public void ClearSelection() {
-		selectionNameText.text = string.Empty;
 		if (actionButtons != null) {
 			foreach (var ab  in actionButtons) {
 				Destroy(ab);
 			}
 			actionButtons.Clear ();
 		}
+//		foreach (var gameObject in selectedWobObjects) {
+//			Destroy (gameObject);
+//		}
+//		selectedWobObjects.Clear ();
 	}
 
 	public bool IsMouseInHUDBounds() {
 		Vector3[] corners = new Vector3[4];
 		actionsPanel.GetLocalCorners (corners);
 		float yMax = 0;
-		Debug.Log ("Anchor : " + actionsPanel.anchoredPosition);
 		foreach (Vector3 corner in corners) {
-			Debug.Log ("Corner : " + corner);
-
 			float offsetY = corner.y + actionsPanel.anchoredPosition.y;
-			// Vector3 offsetCorner = actionsPanel.anchorMax
 			yMax = Mathf.Max (yMax, offsetY);
 		}
 
@@ -70,14 +79,11 @@ public class HUD : MonoBehaviour {
 	}
 
 	void ConfigureHUDForWorldObject(WorldObject wob) {
-		selectionNameText.text = wob.name;
 		AddActions (wob);
 	}
 
 	void AddActions(WorldObject wob) {
 		List<GameObject> buttons = new List<GameObject> (wob.actions.Length);
-//		float x = 0;
-//		float y = actionsPanel.;
 		foreach (var action in wob.actions) {
 			var buttonObj = (GameObject)Instantiate (actionButtonPrefab);
 			buttons.Add (buttonObj);
@@ -96,11 +102,6 @@ public class HUD : MonoBehaviour {
 	}
 
 	public void HandleAction(RTS.Action action) {
-		// for selected units pass action
-		// or
-		// player
-
-		// for now, only handle single selection
 		WorldObject wob = selection[0];
 		wob.PerformAction (action);
 	}
