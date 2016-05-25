95b3292409e16f5da09c18965a6ee5755fc8ac51
Ben Scheirman <ben@scheirman.com>
2016-05-24 22:12:39 -0500

    broke-ass selection

--- Assets/Scripts/WorldObjectSelection.cs
+++ Assets/Scripts/WorldObjectSelection.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class WorldObjectSelection : MonoBehaviour {
 
+	public GameObject selectionIndicatorPrefab;
+
 	Player player;
 
 	void Start () {
@@ -19,17 +21,43 @@ public class WorldObjectSelection : MonoBehaviour {
 
 			WorldObject wob = FindHitObject();
 			if (wob) {
-				Debug.Log ("Hit object: " + wob);
-				if (player.SelectedObject != wob) {
-					player.SelectObject (wob);
-				}
+				SelectObject (wob);
 			} else if (HitGround()) {
-				player.ClearSelection ();
+				ClearSelection ();
 			} else {
 				Debug.Log ("Nada");
 			}
-
 		}
+	}
+
+	void SelectObject(WorldObject wob) {
+		if (player.SelectedObject != wob) {
+			player.SelectObject (wob);
+
+			AddSelectionRing (wob);
+		}
+	}
+
+	void AddSelectionRing(WorldObject wob) {
+		
+		Bounds bounds = new Bounds (wob.transform.position, Vector3.zero);
+		foreach (MeshRenderer r in wob.GetComponentsInChildren<MeshRenderer>()) {
+			bounds.Encapsulate (r.bounds);
+		}
+		float maxSize = Mathf.Max (bounds.size.x, bounds.size.z);
+
+		Vector3 objPosition = bounds.center;
+		objPosition.y = 0.01f;
+
+		var ring = (GameObject)Instantiate (selectionIndicatorPrefab, objPosition, Quaternion.identity);
+
+
+		ring.transform.localScale = new Vector3 (maxSize * 1.75f, 1, maxSize * 1.75f);
+		ring.transform.SetParent (wob.transform);
+	}
+
+	void ClearSelection() {
+		player.ClearSelection ();
 	}
 
 	WorldObject FindHitObject() {
